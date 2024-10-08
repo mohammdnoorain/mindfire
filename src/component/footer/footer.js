916fd717c5d7bf75caf21aac30a395fb7220865e
mohammdnoorain <123652138+mohammdnoorain@users.noreply.github.com>
2024-10-08 17:25:06 +0530

    Update footer.js

--- src/component/footer/footer.js
+++ src/component/footer/footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaInstagram, FaFacebook, FaLinkedin, FaThreads } from "react-icons/fa";
+import { FaInstagram, FaFacebook, FaLinkedin } from "react-icons/fa";
 import '../../style/footer.css'
 const Footer = () => {
 
